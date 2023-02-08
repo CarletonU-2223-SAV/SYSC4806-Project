bc6af18011a6720f96a5f8146f5f78c768564e3c
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2023-02-07 22:19:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SYSC 4806 Project - Group 7
+
+Chosen topic: TBD
+
+### Deadlines
+
+- Milestone 1: March 8 2023
+- Milestone 2: March 22 2023
+- Milestone 3: April 5 2023
---
+++ src/test/java/com/groupseven/sysc4806project/Sysc4806ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupseven.sysc4806project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sysc4806ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
