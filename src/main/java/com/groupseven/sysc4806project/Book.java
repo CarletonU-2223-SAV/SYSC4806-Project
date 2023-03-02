ec6dd9c790cca9cb212ad18b2e3ca9d56f4ae849
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2023-03-02 01:02:38 -0500

    Develop BookService with new image handling

--- src/main/java/com/groupseven/sysc4806project/Book.java
+++ src/main/java/com/groupseven/sysc4806project/Book.java
@@ -3,13 +3,22 @@ package com.groupseven.sysc4806project;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Entity
 public class Book {
+    private static final String IMAGE_DIR = "resources/static/";
+
     private int id;
     private int inventory;
-
-
 
     private String isbn, description, title, author, publisher;
 
@@ -44,14 +53,40 @@ public class Book {
         this.publisher = publisher;
     }
 
-
     @Id
     @GeneratedValue
     public int getId() {
         return id;
     }
 
-    public String image() {return "";}
+    @Transient
+    public String getImage() {
+        return "/" + id + ".png";
+    }
+
+    @Transient
+    public boolean setImage(MultipartFile image) {
+        Path savePath = Paths.get(IMAGE_DIR + id + ".png");
+
+        if (!Files.exists(savePath)) {
+            try {
+                Files.createDirectories(savePath);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        try (InputStream is = image.getInputStream()) {
+            Files.copy(is, savePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
     public int getInventory() {
         return inventory;
     }
