7d6fdc5703f1b584885930b01620cf3061f30fd1
Phuc La <phucla2201@gmail.com>
2023-03-05 20:09:42 -0500

    Create add/edit page, still need some testing

--- src/main/java/com/groupseven/sysc4806project/BookstoreController.java
+++ src/main/java/com/groupseven/sysc4806project/BookstoreController.java
@@ -7,8 +7,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Controller
@@ -39,4 +42,23 @@ public class BookstoreController {
         model.addAttribute("books", books);
         return "index";
     }
+
+    @PostMapping("/add-book")
+    public String add(
+            @RequestParam(defaultValue = "") String isbn,
+            @RequestParam(defaultValue = "") String title,
+            @RequestParam(defaultValue = "") String description,
+            @RequestParam(defaultValue = "") String author,
+            @RequestParam(defaultValue = "") String publisher,
+            @RequestParam(defaultValue = "") Integer inventory,
+            @RequestParam(required = false) MultipartFile image,
+            Model model
+    ) {
+        int book_id = this.bookService.create(isbn,title,description,author,publisher,inventory,image);
+        Optional<Book> added_book = Optional.ofNullable(this.bookService.get(book_id));
+        Book book = added_book.get();
+        model.addAttribute("book", book);
+        return "add-edit";
+    }
 }
+
