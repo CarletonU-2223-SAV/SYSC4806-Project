3b89e32bcf833a96bff4a183320b0eaba6e0802a
Phuc La <phucla2201@gmail.com>
2023-04-04 00:17:39 -0400

    clean up

--- src/test/java/com/groupseven/sysc4806project/ImageControllerTest.java
+++ src/test/java/com/groupseven/sysc4806project/ImageControllerTest.java
@@ -9,9 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.File;
-import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
