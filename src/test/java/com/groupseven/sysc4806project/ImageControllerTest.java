a00931fdada7af89f391527bc1b94412e5eb11ce
Phuc La <61946772+phucla0122@users.noreply.github.com>
2023-04-04 15:48:35 -0400

    Image test (#57)
    * Handled image controller

---
+++ src/test/java/com/groupseven/sysc4806project/ImageControllerTest.java
@@ -0,0 +1,39 @@
+package com.groupseven.sysc4806project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.File;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ImageControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ImageService imageService;
+
+    @Test
+    public void cover() throws Exception {
+        Integer bookId = 1;
+        File imageFile = new File("src/test/resources/default.png");
+        when(imageService.coverExists(bookId)).thenReturn(true);
+        when(imageService.getImageFile(bookId)).thenReturn(imageFile);
+        this.mockMvc.perform(get("/book-img/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.IMAGE_PNG));
+    }
+}
